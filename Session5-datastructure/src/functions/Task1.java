338d3c847b10b42ccf8966c1103ac11cde1bccae
rezash86 <shalchian.r@gmail.com>
2020-02-12 20:22:47 -0500

    Update Task1.java

--- Session5-datastructure/src/functions/Task1.java
+++ Session5-datastructure/src/functions/Task1.java
@@ -37,4 +37,78 @@ public class Task1 {
 		}
 		return base * power(base, n-1);
 	}
+	
+	public int findMax(int[] arr, int n) {
+		//try to put out one element of the list
+		// and compare it with the whole
+		if(n==1) {
+			return arr[0];
+		}
+		
+		return Math.max(arr[n-1], findMax(arr, n-1));
+	}
+	
+	public int binarySearchRec(int[] arr, int first, int last, int key) {
+		
+		if(first<=last) {
+			int middle = (first + last )/2;
+			
+			if(arr[middle] == key) {
+				return middle;
+			}
+			
+			if(arr[middle] > key) {
+				return binarySearchRec(arr, first, middle - 1, key);
+			}
+			
+			return binarySearchRec(arr, middle + 1, last, key);
+
+		}
+		
+		return -1;
+	}
+	
+	//chihichi
+	public int countHi(String str) {
+		//exit condition
+		if (str.length() < 2) {
+			return 0;
+		}
+		
+		//check the count
+		if(str.substring(0, 2).equals("hi")) {
+			return 1 + countHi(str.substring(2));
+		}
+		
+		//if it wasn't hi, we removed the first character
+		return countHi(str.substring(1));
+	}
+	
+	public String changeXY(String str) {
+		if (str.length() == 0) {
+			return "";
+		}
+		
+		if (str.charAt(0) == 'X') {
+			return 'Y' + changeXY(str.substring(1));
+		}
+		return str.charAt(0) + changeXY(str.substring(1));
+	}
+	
+	//FXYUUUX
+	public String removeX(String str) {
+		//imagin you have only one X and another char
+		if(str.length() < 1) {
+			return str;
+		}
+		
+		if (str.charAt(0) == 'X') {
+			return removeX(str.substring(1));
+		}
+		return str.charAt(0) + removeX(str.substring(1));
+		
+	}
+	
+	
+	
 }
